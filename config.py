d3f9f17999be579ffc7824e2c65470707a92e6df
vishal <subhavishal@yahoo.co.in>
2021-11-28 21:38:23 +0530

    gridsearch tuning

--- config.py
+++ config.py
@@ -3,10 +3,10 @@ acrobot = {"alpha" : 1, "beta" : 0.2, "epsilon" : 0,"no_actions" : 3}
 
 taxi = {"alpha" : 1, "beta" : 0.2, "epsilon" : 0,"no_actions" : 3}
 
-kbca = {"alpha" : 1, "beta" : 0.2, "epsilon" : 0.3,"no_actions" : 2}
+kbca = {'alpha': 0.9333333333333333, 'beta': 0.4368421052631579, 'epsilon': 0.3526315789473684, 'no_actions': 2}
 
-kbcb = {"alpha" : 1, "beta" : 0.2, "epsilon" : 0.3,"no_actions" : 2}
+kbcb = {'alpha': 0.9333333333333333, 'beta': 0.4368421052631579, 'epsilon': 0.3526315789473684, 'no_actions': 2}
 
-kbcc = {"alpha" : 1, "beta" : 0.2, "epsilon" : 0.3,"no_actions" : 2}
+kbcc = {'alpha': 0.9333333333333333, 'beta': 0.4368421052631579, 'epsilon': 0.3526315789473684, 'no_actions': 2}
 
 config = {"acrobot":acrobot, "taxi": taxi, "kbca": kbca, "kbcb": kbcb, "kbcc": kbcc}
