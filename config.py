c33f6009a3bb66d175f7eaaa00eac11312bce511
vishal <subhavishal@yahoo.co.in>
2021-11-30 10:35:41 +0530

    taxi tuned

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 # Storing hyperparameters
-acrobot = {"alpha" : 1, "beta" : 0.1, "epsilon" : 0.9,"no_actions" : 3}
+acrobot = {"alpha" : 1, "beta" : 0.9, "epsilon" : 0.5,"no_actions" : 3}
 
-taxi = {"alpha" : 1, "beta" : 0.2, "epsilon" : 0.91,"no_actions" : 6}
+taxi = {'alpha': 0.99, 'beta': 0.2, 'epsilon': 0.89, 'no_actions': 6}
 
-kbca = {'alpha': 0.9333333333333333, 'beta': 0.4368421052631579, 'epsilon': 0.3526315789473684, 'no_actions': 2}
+kbca = {'alpha': 1.0, 'beta': 0.67, 'epsilon': 0.81, 'no_actions': 2}
 
 kbcb = {'alpha': 1.0, 'beta': 0.39473684210526316, 'epsilon': 0.45789473684210524, 'no_actions': 2}
 
